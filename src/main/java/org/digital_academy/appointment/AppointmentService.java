d2e93367efd3f4bffd7afe8211406ae536a1a10f
Efren TC <efrentomascampa@gmail.com>
2025-09-15 15:35:29 +0200

    feat: I create services for appointment, patient and user

--- src/main/java/org/digital_academy/appointment/AppointmentService.java
+++ src/main/java/org/digital_academy/appointment/AppointmentService.java
@@ -1,5 +1,35 @@
 package org.digital_academy.appointment;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class AppointmentService {
 
+    private final AppointmentRepository appointmentRepository;
+
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
+
+    public Optional<Appointment> getAppointmentById(Long id) {
+        return appointmentRepository.findById(id);
+    }
+
+    public List<Appointment> getAppointmentsByPatient(Long patientId) {
+        return appointmentRepository.findByPatientId(patientId);
+    }
+
+    public Appointment createAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    public void deleteAppointment(Long id) {
+        appointmentRepository.deleteById(id);
+    }
 }
