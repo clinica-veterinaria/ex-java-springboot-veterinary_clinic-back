faa46722c3236acf083ef4b45f6244f25406c8ee
Efren TC <efrentomascampa@gmail.com>
2025-09-22 17:41:28 +0200

    feat: enable scheduling functionality

--- src/main/java/org/digital_academy/ClinicaveterinariaApplication.java
+++ src/main/java/org/digital_academy/ClinicaveterinariaApplication.java
@@ -2,7 +2,9 @@ package org.digital_academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class ClinicaveterinariaApplication {
     public static void main(String[] args) {
