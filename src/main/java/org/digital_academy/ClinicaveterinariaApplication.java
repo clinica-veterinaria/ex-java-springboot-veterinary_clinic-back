8fc517e23a11d855a561fba3a62fa2b00b1a38cb
Efren TC <efrentomascampa@gmail.com>
2025-09-22 14:30:52 +0200

    refactor: relocate main method to the top-level

---
+++ src/main/java/org/digital_academy/ClinicaveterinariaApplication.java
@@ -0,0 +1,11 @@
+package org.digital_academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaveterinariaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaveterinariaApplication.class, args);
+    }
+}
