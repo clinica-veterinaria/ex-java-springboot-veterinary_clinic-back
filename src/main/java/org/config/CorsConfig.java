82bedd4b9ac990b65bf78ee75279c1950decb045
Paula Fernandez Aranda <paulafa8@gmail.com>
2025-09-25 12:38:18 +0200

    connection working

--- src/main/java/org/config/CorsConfig.java
+++ src/main/java/org/config/CorsConfig.java
@@ -3,11 +3,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.lang.NonNull;
+
+
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
                 .allowedOrigins("http://localhost:5173") // Allow requests from your frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
