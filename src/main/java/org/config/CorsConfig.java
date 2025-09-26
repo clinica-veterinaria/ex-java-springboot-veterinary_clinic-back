59ecaa7d1cd95fa8a7560b79b29f5021e404a254
Sara Vázquez <saravazlo@gmail.com>
2025-09-26 13:56:56 +0200

    fix: changes in security

--- src/main/java/org/config/CorsConfig.java
+++ src/main/java/org/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@NonNull CorsRegistry registry) {
         registry.addMapping("/**") // Apply CORS to all endpoints
-                .allowedOrigins("http://localhost:5173") // Allow requests from your frontend's origin
+                .allowedOrigins("http://localhost:5173") // frontend
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
                 .allowedHeaders("*"); // Allow all headers
     }
