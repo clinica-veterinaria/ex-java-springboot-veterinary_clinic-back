88976c60974df657fe0e70036454c8868bdaf1bd
EfrenTC <efrentomascampa@gmail.com>
2025-09-30 14:32:38 +0200

    feature: cors en authcontroller

--- src/main/java/org/digital_academy/user/AuthController.java
+++ src/main/java/org/digital_academy/user/AuthController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class AuthController {
 
     private final UserRepository userRepository;
